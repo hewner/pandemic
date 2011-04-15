31b95e8b837676ecbc74083afea30eee085936e3
hewner@gmail.com <hewner@gmail.com@b33b9599-90f7-8623-b8ff-551778ae6848>
2011-04-15 20:28:02 +0000

    Cure diseases

---
+++ Pandemic/Pandemic/CureDiseaseAction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pandemic
+{
+    public class CureDiseaseAction : Action
+    {
+        DiseaseColor color;
+        private CureDiseaseAction(DiseaseColor color)
+        {
+            this.color = color;
+        }
+
+        public override GameState execute(GameState gs)
+        {
+            Player player = gs.currentPlayer();
+            int cardsRemoved = 0;
+
+            foreach (City card in player.cards)
+            {
+                if (cardsRemoved == 5) break;
+                if (color == card.color)
+                {
+                    player = player.removeCard(card);
+                    cardsRemoved++;
+                }
+            }
+            GameState result = gs.cureDisease(color);
+            result = result.adjustPlayer(player);
+            result.advanceMove();
+            return result;
+
+        }
+
+        public static List<CureDiseaseAction> getCureActions(GameState g)
+        {
+            List<CureDiseaseAction> result = new List<CureDiseaseAction>();
+            if(g.map.hasStation(g.currentPlayer().position)) {
+                foreach(DiseaseColor c in g.currentPlayer().hasCardsToCure())
+                {
+                    if (g.curesFound[(int) c]) continue;
+
+                    result.Add(new CureDiseaseAction(c));
+                }
+            }
+            return result;
+        }
+    }
+}
